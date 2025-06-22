eaf8824385621cd5f7554beaa37d68821eaaa019
Akshata Bhoi <142676478+AkshataBhoi@users.noreply.github.com>
2025-06-22 11:05:34 +0530

    Update oops.java

--- oops.java
+++ oops.java
@@ -5,8 +5,8 @@ class Pen {
     String type;
 
     public void Writing() {
-        System.out.println("Color:" + color);
-        System.out.println("Type:" + type);
+        System.out.println("Color:" + this.color);
+        System.out.println("Type:" + this.type);
         System.out.println("Write Something");
     }
 }
@@ -18,5 +18,10 @@ public class oops {
         pen1.color = "Black";
         pen1.type = "Ballpoint";
         pen1.Writing();
+        Pen pen2 = new Pen();
+        pen2.color = "Blue";
+        pen2.type = "Gel Point";
+        pen1.Writing();
+        
     }
 }
